1ed0f88b41e87efaf0da9263166803bb568461c1
Kevin Araújo <kfa_34@hotmail.com>
2024-01-11 16:55:55 -0300

    feat: Created EnderecoEntity and created a relationship one to one with AdotanteEntity.

--- src/entities/AdotanteEntity.ts
+++ src/entities/AdotanteEntity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import EnderecoEntity from "./EnderecoEntity";
 
 @Entity()
 export default class AdotanteEntity {
@@ -17,15 +18,16 @@ export default class AdotanteEntity {
     @Column({ nullable: true })
     foto?: string;
 
-    @Column({ nullable: true })
-    endereco?: string;
+    @OneToOne(() => EnderecoEntity, { nullable: true, cascade: true, eager: true })
+    @JoinColumn()
+    endereco?: EnderecoEntity;
 
     constructor(
         nome: string,
         senha: string,
         celular: string,
         foto?: string,
-        endereco?: string
+        endereco?: EnderecoEntity
     ) {
         this.nome = nome;
         this.senha = senha;
